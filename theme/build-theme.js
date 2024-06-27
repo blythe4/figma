205fa135afbe8c4d95d3e41072bc3f98121e5d66
황수영 hwangsuyeong <syhwang@commerceon.co.kr>
2024-06-27 11:49:50 +0900

    added tailwind content

--- theme/build-theme.js
+++ theme/build-theme.js
@@ -6,6 +6,15 @@ const StyleDictionary = StyleDictionaryModule.extend(
     type: "all",
     source: ["theme/tokenStudio.json"], // 토큰 스튜디오에서 만들어준 json 파일 경로
     buildPath: "",
+    tailwind: {
+      content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/provider/**/*.{js,ts,jsx,tsx,mdx}",
+      ],
+    },
   }),
 );
 
